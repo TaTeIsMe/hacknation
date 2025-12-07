34a46f5d62754748bd27e776ea37ee87effb9759
Kacper <48856951+n0tcnj1eck4h@users.noreply.github.com>
2025-12-07 05:17:56 +0100

    radar gowno

--- minimap.gd
+++ minimap.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
-func update_minimap(room: Room, wizard_room: Room, pos = Vector2i.ZERO, orientation=Vector2i.UP):
-	$Icons.erase_cell(pos)
+func rotate_right(vec: Vector2i) -> Vector2i:
+	return Vector2i(-vec.y, vec.x)
+
+# Rotate left (counter-clockwise) 90°
+func rotate_left(vec: Vector2i) -> Vector2i:
+	return Vector2i(vec.y, -vec.x)
+	
+func update_minimap(room: Room, wizard_room: Room, pos = Vector2i.ZERO, orientation=Vector2i.LEFT, clear: bool = true):
+	if clear:
+		$Mask/Icons.clear()
+		$Mask/BG.clear()
 	
 	if room.visited && room.contents != null && room.contents.tile_pos() != Vector2i.ZERO:
 		$Icons.set_cell(pos, 0, room.contents.tile_pos())
@@ -18,41 +27,34 @@ func update_minimap(room: Room, wizard_room: Room, pos = Vector2i.ZERO, orientat
 	
 	var w = [Vector2i(7,0), Vector2i(5,0),Vector2i(6,0),Vector2i(4,0)]
 	if room == wizard_room:
-		$Icons.set_cell(pos, 0, w[idx])
+		$Mask/Icons.set_cell(pos, 0, w[idx])
 	
 	var mask = [room.right != null, room.front != null, room.left != null]
 	var t = [Vector2i(3,0), Vector2i(3,1), Vector2i(2,1), Vector2i(2,0)]
-	var l = [Vector2i(1,1), Vector2i(0,1), Vector2i(0,2), Vector2i(1,2)]
+	var l = [Vector2i(0,1), Vector2i(1,1), Vector2i(1,2), Vector2i(0,2)]
 	var d = [Vector2i(3,2), Vector2i(3,3), Vector2i(2,3), Vector2i(2,2)]
 	var s = [Vector2i(1,3), Vector2i(0,3)]
 	var b = [Vector2i(4,3), Vector2i(4,2), Vector2i(5,2), Vector2i(5,3)]
 	if !room.visited:
-		$BG.set_cell(pos, 0, b[idx])
+		$Mask/BG.set_cell(pos, 0, b[idx])
 		return
 	if mask == [true, true, true]:
-		$BG.set_cell(pos, 0, Vector2i(6,3))
+		$Mask/BG.set_cell(pos, 0, Vector2i(6,3))
 	elif mask == [true, false, true]:
-		$BG.set_cell(pos, 0, t[idx])
+		$Mask/BG.set_cell(pos, 0, t[idx])
 	elif mask == [true, false, false] || mask == [false, false, true] :
-		$BG.set_cell(pos, 0, l[idx])
+		$Mask/BG.set_cell(pos, 0, l[idx])
 	elif mask == [false, false, false]:
-		$BG.set_cell(pos, 0, d[idx])
+		$Mask/BG.set_cell(pos, 0, d[idx])
 	elif mask == [false, true, false]:
-		$BG.set_cell(pos, 0, s[idx% 2])
+		$Mask/BG.set_cell(pos, 0, s[idx% 2])
 		
 	if room.left != null:
-		var left_dir = Vector2i(orientation.y, -orientation.x)
-		self.update_minimap(room.left, wizard_room, pos + left_dir, left_dir)
+		var left_dir = rotate_left(orientation)
+		self.update_minimap(room.left, wizard_room, pos + left_dir, left_dir, false)
 	if room.right != null:
-		var right_dir = Vector2i(-orientation.y, orientation.x)
-		self.update_minimap(room.right,wizard_room,  pos + right_dir, right_dir)
+		var right_dir = rotate_right(orientation)
+		self.update_minimap(room.right,wizard_room,  pos + right_dir, right_dir, false)
 	if room.front != null:
-		self.update_minimap(room.front,wizard_room,  pos + orientation, orientation)
+		self.update_minimap(room.front,wizard_room,  pos + orientation, orientation, false)
 	
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
--- navigator.gd
+++ navigator.gd
@@ -1,7 +1,14 @@
 extends Node2D
+# todo: przetestować zakęty, maska na minmapę, odddzielić minimapę
+# obrót minimapy, czarny syf 
+var head = Room.new(
+		null, 
+			null, 
+	Room.new(null,null,Room.new(null,Room.new(Room.new(),null,Room.new()),null),), 
+)
 
-var head = Room.new(Room.new(), null, Room.new(Room.new(),null,Room.new(Room.new(null,Room.new())), Room.RoomLoot.new()))
-var position_stack: Array = [head]
+
+var position_stack: Array = [[head, Vector2i.LEFT, Vector2i.ZERO]]
 
 const start_tile = Vector2i(6,3)
 const gamba_tile = Vector2i(5,3)
@@ -11,20 +18,30 @@ const wizard_tile = Vector2i(5,0)
 const visited_tile = Vector2i(0,0)
 const unvisited_tile = Vector2i(1,0)
 
+func get_current_room() -> Room:
+	return self.position_stack[-1][0]
+	
+func get_current_orientation() -> Vector2i:
+	return self.position_stack[-1][1]
+	
+func get_current_position() -> Vector2i:
+	return self.position_stack[-1][2]
 
 func update_scene():
+	print(self.get_current_position())
+	print(self.get_current_orientation())
 	$BackButton.disabled = self.position_stack.size() <= 1
 	$BackButton.visible = self.position_stack.size() > 1
 	
-	$RightButton.disabled = self.position_stack[-1].right == null
-	$FrontButton.disabled = self.position_stack[-1].front == null
-	$LeftButton.disabled = self.position_stack[-1].left == null
-	$RightButton.visible = self.position_stack[-1].right != null
-	$FrontButton.visible = self.position_stack[-1].front != null
-	$LeftButton.visible = self.position_stack[-1].left != null
+	$RightButton.disabled = self.get_current_room().right == null
+	$FrontButton.disabled = self.get_current_room().front == null
+	$LeftButton.disabled = self.get_current_room().left == null
+	$RightButton.visible = self.get_current_room().right != null
+	$FrontButton.visible = self.get_current_room().front != null
+	$LeftButton.visible = self.get_current_room().left != null
 	
-	var room = self.position_stack[-1]	
-	for i in range(4):
+	var room = self.get_current_room()
+	for i in range($Node2D/Decorations.get_children().size()):
 		$Node2D/Decorations.get_children()[i].visible = room.decorations[i] != Room.Decoration.NO
 		$Node2D/Decorations.get_children()[i].flip_h = room.decorations[i] == Room.Decoration.FLIPPED
 	
@@ -56,26 +73,47 @@ func update_scene():
 			$Node2D/Background.texture = preload("res://Resources/Corridors/wall4.png")
 		[true, true, true]:
 			$Node2D/Background.texture = preload("res://Resources/Corridors/wall8.png")
-	$Minimap.update_minimap(self.head, self.position_stack[-1])
+	$Minimap.update_minimap(self.head, room, -self.get_current_position())
 
 func _ready() -> void:
 	self.head.visited = true
 	self.update_scene()
 	pass # Replace with function body.
+	
+func rotate_right(vec: Vector2i) -> Vector2i:
+	return Vector2i(-vec.y, vec.x)
 
+# Rotate left (counter-clockwise) 90°
+func rotate_left(vec: Vector2i) -> Vector2i:
+	return Vector2i(vec.y, -vec.x)
+	
 func _go(where: String) -> void:
 	if where == "back":
 		if self.position_stack.size() > 1:
 			self.position_stack.pop_back()
 	elif where == "right":
-		self.position_stack.push_back(self.position_stack[-1].right)
+		var orientation = rotate_right(self.get_current_orientation())
+		self.position_stack.push_back([
+			self.get_current_room().right, 
+			orientation,
+			self.get_current_position() + orientation
+			])
 	elif where == "front":
-		self.position_stack.push_back(self.position_stack[-1].front)
+		self.position_stack.push_back([
+			self.get_current_room().front, 
+			self.get_current_orientation(),
+			self.get_current_position() + self.get_current_orientation()
+			])
 	elif where == "left":
-		self.position_stack.push_back(self.position_stack[-1].left)
+		var orientation = rotate_left(self.get_current_orientation())
+		self.position_stack.push_back([
+			self.get_current_room().left, 
+			orientation,
+			self.get_current_position() + orientation
+			])
 	else:
 		printerr("gdzie ty idziesz?? ", where)
-	self.position_stack[-1].visited = true
+	self.get_current_room().visited = true
 	self.update_scene()
 		
 		
