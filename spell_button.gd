b6afb817140d773969f88a6de57d3f91ee2c5bd1
vbinudx <73001346+vbinudx@users.noreply.github.com>
2025-12-07 03:18:32 +0100

    small adjustments

--- spell_button.gd
+++ spell_button.gd
@@ -1,13 +1,64 @@
 extends Button
 
-@export var button_text: String = "Button"
+enum SpellKind {
+	Fire,
+	Ice,
+	Spark,
+	Fireball
+}
+
+@export var spell_kind: SpellKind = SpellKind.Fire
 @export var spell_accuracy: float = 20.0
 @export var spell_power: float = 10.0
 
+var tween_stylebox:StyleBoxFlat
+var styleboxes:Dictionary = {}
+var current_state = BaseButton.DRAW_NORMAL
+
+var tween:Tween
+
+signal spells_vars(spell_type)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.text = button_text
+	match spell_kind:
+		SpellKind.Fire:
+			self.text = "Oktażar"
+		SpellKind.Ice:
+			self.text = "Tetraszron"
+		SpellKind.Spark:
+			self.text = "Ikosagrom"
+		SpellKind.Fireball:
+			self.text = "Kula Ognia"
+			
+	self.pressed.connect(_button_pressed)
+	
+	tween_stylebox = get_theme_stylebox('normal').duplicate()
+	
+	styleboxes[BaseButton.DRAW_NORMAL] = get_theme_stylebox('normal').duplicate()
+	styleboxes[BaseButton.DRAW_HOVER] = get_theme_stylebox('hover').duplicate()
+	styleboxes[BaseButton.DRAW_PRESSED] = get_theme_stylebox('pressed').duplicate()
+	styleboxes[BaseButton.DRAW_HOVER_PRESSED] = get_theme_stylebox('pressed').duplicate()
+	
+	add_theme_stylebox_override('normal', tween_stylebox)
+	add_theme_stylebox_override('hover', tween_stylebox)
+	add_theme_stylebox_override('pressed', tween_stylebox)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if get_draw_mode() != current_state:
+		# If the draw mode changed
+		current_state = get_draw_mode()
+		# Kill the running tween
+		if tween and tween.is_running():
+			tween.kill()
+		# And create a new one
+		tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC).set_parallel()
+		# That tweens some properties of our tween stylebox to the target stylebox
+		# depending on the current state
+		var target = styleboxes[current_state] as StyleBoxFlat
+		tween.tween_property(tween_stylebox, "bg_color", target.bg_color, 0.2)
+		tween.tween_property(tween_stylebox, "border_color", target.border_color, 0.2)
+
+func _button_pressed():
+	spells_vars.emit(spell_kind)
