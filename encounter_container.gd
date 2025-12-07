07b81880b2c779dca6dfe435feb28c327cd0fc63
TaTeIsMe <129204050+TaTeIsMe@users.noreply.github.com>
2025-12-07 07:16:28 +0100

    killed slam

--- encounter_container.gd
+++ encounter_container.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 
 const ENCOUNTER = preload("res://encounter.tscn")
+signal enemy_died
 
 func _on_navigator_start_encounter(kind: Encounter.EnemyKind) -> void:
 	var encounter = ENCOUNTER.instantiate()
 	encounter.enemy_kind = kind
+	encounter.enemy_died.connect(_on_enemy_ded)
 	self.add_child(encounter)
+
+func _on_enemy_ded()-> void:
+	for n in get_children():
+		remove_child(n)
+	enemy_died.emit()
