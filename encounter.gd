8f9bb0c48fb069f55ac240446a86a547db97d079
vbinudx <73001346+vbinudx@users.noreply.github.com>
2025-12-07 02:13:27 +0100

    Added encounter data

---
+++ encounter.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+enum EnemyKind {
+	Glucior,
+	Minotaur,
+	Wizard
+}
+
+@export var encounter_hp: float = 100.0
+@export var enemy_kind: EnemyKind = EnemyKind.Glucior
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	match enemy_kind:
+		EnemyKind.Glucior:
+			var sprite: Texture2D = preload("res://Resources/slime.png")
+			self.get_node("Encounter Sprite").texture = sprite
+			self.get_node("Encounter Label").text = "Glucior"
+		EnemyKind.Minotaur:
+			var sprite: Texture2D = preload("res://Resources/minotour.png")
+			self.get_node("Encounter Sprite").texture = sprite
+			self.get_node("Encounter Label").text = "Gigancka\nMućka"
+		EnemyKind.Wizard:
+			var sprite: Texture2D = preload("res://Resources/wizard.png")
+			self.get_node("Encounter Sprite").texture = sprite
+			self.get_node("Encounter Label").text = "Wiedźmak\nStrahenstein"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
